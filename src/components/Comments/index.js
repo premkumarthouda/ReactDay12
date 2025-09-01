53f3e08252f132315974a54e5d66e7608dcaa170
Prem Kumar <113364219+premkumarthouda@users.noreply.github.com>
2025-09-01 21:28:27 +0530

    Update index.js

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,3 +1,13 @@
+import {Component} from 'react'
+
+import {formatDistanceToNow} from 'date-fns'
+
+import {v4} from 'uuid'
+
+import CommentItem from '../CommentItem'
+
+import './index.css'
+
 const initialContainerBackgroundClassNames = [
   'amber',
   'blue',
@@ -9,23 +19,94 @@ const initialContainerBackgroundClassNames = [
 ]
 
 // Write your code here
-import {Component} from 'react'
 
 class Comments extends Component {
+  state = {
+    name: '',
+    comment: '',
+    commentsList: [],
+  }
+
+  inputName = event => {
+    this.setState({name: event.target.value})
+    console.log(event.target.value)
+  }
+
+  inputComment = event => {
+    this.setState({comment: event.target.value})
+    console.log(event.target.value)
+  }
+
+  addComment = event => {
+    event.preventDefault()
+    const {name, comment} = this.state
+    const backgroundClassNames =
+      initialContainerBackgroundClassNames[
+        Math.ceil(Math.random() * initialContainerBackgroundClassNames.length)
+      ]
+    const newComment = {
+      id: v4(),
+      name,
+      comment,
+      time: new Date(),
+    }
+    this.setState(prevState => ({
+      commentsList: [...prevState.commentsList, newComment],
+      name: '',
+      comment: '',
+    }))
+  }
+
+  renderComment = () => {
+    const {commentsList} = this.state
+    return commentsList.map(eachItem => (
+      <CommentItem eachDetails={eachItem} key={eachItem.id} />
+    ))
+  }
+
   render() {
+    const {name, comment} = this.state
     return (
-      <div>
-        <div>
-          <h1>Comments</h1>
-          <p>Say something about 4.0 Technologies</p>
+      <div className="app-container">
+        <h1 className="heading">Comments</h1>
+        <div className="input-container">
+          <form className="mini-container" onSubmit={this.addComment}>
+            <h1 className="form-description ">
+              Say something about 4.0 Technologies
+            </h1>
 
-          <input type="text" placeholder="Your Name"/>
-          <input type="textarea" placeholder="Your Comments " />
+            <input
+              type="text"
+              placeholder="Your Name"
+              className="name-input"
+              onChange={this.inputName}
+              value={name}
+            />
+            <input
+              type="textarea"
+              placeholder="Your Comments "
+              className="comment-input"
+              onChange={this.inputComment}
+              value={comment}
+            />
 
-          <button>Add Comment</button>
+            <button type="submit" className="add-button">
+              Add Comment
+            </button>
+          </form>
+          <div>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+              className="image"
+            />
+          </div>
+        </div>
+        <div className="comment-list">
+          <p className="add-button">0</p>
+          <h1 className="head">Comments</h1>
         </div>
         <div>
-        <img src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"/>
+          <ul>{this.renderComment()}</ul>
         </div>
       </div>
     )
