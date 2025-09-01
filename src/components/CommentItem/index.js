08bf9daef05a03603d73a608bab51095248e04e1
Prem Kumar <113364219+premkumarthouda@users.noreply.github.com>
2025-09-01 21:29:16 +0530

    Update index.js

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,13 @@
 // Write your code here
+import React from 'react'
+const CommentItem = props => {
+  const {eachDetails} = props
+  const {name, comment} = eachDetails
+  return (
+    <li>
+      <p>{name}</p>
+      <p>{comment}</p>
+    </li>
+  )
+}
+export default CommentItem
