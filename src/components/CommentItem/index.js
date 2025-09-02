bdf03a37424383b82eda9587fd5bd66b8b2cbf41
Prem Kumar <113364219+premkumarthouda@users.noreply.github.com>
2025-09-02 07:45:36 +0530

    Update index.js

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,12 +1,56 @@
 // Write your code here
+import {formatDistanceToNow} from 'date-fns'
+import './index.css'
 import React from 'react'
+
 const CommentItem = props => {
-  const {eachDetails} = props
-  const {name, comment} = eachDetails
+  const {eachDetails, onClickLike, onDelete} = props
+  const {id, name, comment, time, isLiked, backgroundClassNames} = eachDetails
+  const isOnClikcLiked = () => {
+    onClickLike(id)
+  }
+
+  const profile = name.toUpperCase()[0]
+
   return (
-    <li>
-      <p>{name}</p>
-      <p>{comment}</p>
+    <li className="list">
+      <div className="top-container">
+        <p className={backgroundClassNames}> {profile}</p>
+        <div>
+          <h1 className="Name">{name}</h1>
+          <p className="Comment">{comment}</p>
+        </div>
+        <p className="date">
+          {formatDistanceToNow(new Date(time))} minuties ago
+        </p>
+      </div>
+
+      <div className="btn-contaner">
+        <div className="like-container">
+          <img
+            src={
+              isLiked
+                ? 'https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png'
+                : 'https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png'
+            }
+            className="like-img"
+            alt="like"
+          />
+          <button
+            className={isLiked ? 'para-like' : 'para'}
+            onClick={isOnClikcLiked}
+          >
+            Like
+          </button>
+        </div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"
+          className="delete"
+          onClick={() => onDelete(id)}
+          data-testid="delete"
+          alt="delete"
+        />
+      </div>
     </li>
   )
 }
