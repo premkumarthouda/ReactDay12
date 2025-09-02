276805026bb802de13767139ff60bc068c77770f
Prem Kumar <113364219+premkumarthouda@users.noreply.github.com>
2025-09-02 07:46:20 +0530

    Update index.js

--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -37,18 +37,40 @@ class Comments extends Component {
     console.log(event.target.value)
   }
 
+  onClickLike = id => {
+    this.setState(prevState => ({
+      commentsList: prevState.commentsList.map(eachItem =>
+        id === eachItem.id
+          ? {...eachItem, isLiked: !eachItem.isLiked}
+          : eachItem,
+      ),
+    }))
+  }
+
+  onDelete = id => {
+    const {commentsList} = this.state
+    const filterCommentsList = commentsList.filter(eachItem => {
+      id !== eachItem.id
+    })
+    this.setState({commentsList: filterCommentsList})
+  }
+
   addComment = event => {
     event.preventDefault()
     const {name, comment} = this.state
-    const backgroundClassNames =
+    const backgroundClassNames = `char-profie ${
       initialContainerBackgroundClassNames[
         Math.ceil(Math.random() * initialContainerBackgroundClassNames.length)
       ]
+    }`
+
     const newComment = {
       id: v4(),
       name,
       comment,
       time: new Date(),
+      backgroundClassNames,
+      isLiked: false,
     }
     this.setState(prevState => ({
       commentsList: [...prevState.commentsList, newComment],
@@ -60,20 +82,25 @@ class Comments extends Component {
   renderComment = () => {
     const {commentsList} = this.state
     return commentsList.map(eachItem => (
-      <CommentItem eachDetails={eachItem} key={eachItem.id} />
+      <CommentItem
+        eachDetails={eachItem}
+        key={eachItem.id}
+        onClickLike={this.onClickLike}
+        onDelete={this.onDelete}
+      />
     ))
   }
 
   render() {
-    const {name, comment} = this.state
+    const {name, comment, commentsList} = this.state
     return (
       <div className="app-container">
         <h1 className="heading">Comments</h1>
         <div className="input-container">
           <form className="mini-container" onSubmit={this.addComment}>
-            <h1 className="form-description ">
-              Say something about 4.0 Technologies
-            </h1>
+            <p className="form-description ">
+              Say Something about 4.0 Technologies
+            </p>
 
             <input
               type="text"
@@ -82,8 +109,7 @@ class Comments extends Component {
               onChange={this.inputName}
               value={name}
             />
-            <input
-              type="textarea"
+            <textarea
               placeholder="Your Comments "
               className="comment-input"
               onChange={this.inputComment}
@@ -98,15 +124,16 @@ class Comments extends Component {
             <img
               src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
               className="image"
+              alt="comments"
             />
           </div>
         </div>
         <div className="comment-list">
-          <p className="add-button">0</p>
-          <h1 className="head">Comments</h1>
+          <p className="add-button">{commentsList.length}</p>
+          <p className="head">Comments</p>
         </div>
         <div>
-          <ul>{this.renderComment()}</ul>
+          <ul className="un-list">{this.renderComment()}</ul>
         </div>
       </div>
     )
